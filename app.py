6c6bd4a418f061d4b3554f100e4030fc1a6f159a
manishlawande1906 <128923470+manishlawande1906@users.noreply.github.com>
2025-11-08 21:00:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -173,13 +173,13 @@ col1, col2 = st.columns(2)
 with col1:
     st.image("zomato_dashboard.png", caption="Zomato Dashboard Overview", use_container_width=True)
 with col2:
-    st.image("zomato_city_performace.png", caption="City Performance Analysis", use_container_width=True)
+    st.image("zomato_overview.png", caption="Sales & Category Overview", use_container_width=True)
 
 col3, col4 = st.columns(2)
 with col3:
     st.image("zomato_user_performance.png", caption="User Performance Analysis", use_container_width=True)
 with col4:
-    st.image("zomato_overview.png", caption="Sales & Category Overview", use_container_width=True)
+    st.image("zomato_city_performace.png", caption="City Performance Analysis", use_container_width=True)
 
 # --- Stylish Google Drive link section ---
 st.markdown(
